97f718c8ae8e9c023280ff2f9d5ed23e2f5c60e9
Maksim Koshman <maksimkoshman090@gmail.com>
2020-06-14 13:05:16 +0300

    fixed bags

--- js/_custom.js
+++ js/_custom.js
@@ -56,6 +56,10 @@ $(".employees__container").each(function () {
       el: ".employees__progress",
       type: "progressbar",
     },
+    navigation: {
+      nextEl: ".employees__next",
+      prevEl: ".employees__prev",
+    },
     breakpoints: {
       1199: {
         slidesPerView: 4,
@@ -71,50 +75,60 @@ $(".employees__container").each(function () {
 });
 
 // Project Slider
-var projectsSlider = new Swiper(".projects__container", {
-  slidesPerView: 2.6,
-  spaceBetween: 30,
-  loop: true,
-  navigation: {
-    nextEl: ".projects__next",
-    prevEl: ".projects__prev",
-  },
-  pagination: {
-    el: ".projects__progress",
-    type: "progressbar",
-  },
-  breakpoints: {
-    1199: {
-      slidesPerView: 2.6,
-    },
-    767: {
-      slidesPerView: 2.6,
-    },
-    320: {
-      slidesPerView: 1.2,
-    },
-  },
-});
-
-// $(".projects__container").each(function () {
-//   let $this = $(this);
-
-//   new Swiper($this, {
-//     grabCursor: true,
-//     freeMode: true,
-//     breakpoints: {
-//       1199: {
-//         slidesPerView: 1,
-//       },
-//       767: {
-//         slidesPerView: 1,
-//       },
-//       320: {
-//         slidesPerView: 1,
-//       },
+// var projectsSlider = new Swiper(".projects__container", {
+//   slidesPerView: 2.6,
+//   spaceBetween: 30,
+//   loop: true,
+//   navigation: {
+//     nextEl: ".projects__next",
+//     prevEl: ".projects__prev",
+//   },
+//   pagination: {
+//     el: ".projects__progress",
+//     type: "progressbar",
+//   },
+//   breakpoints: {
+//     1199: {
+//       slidesPerView: 2.6,
 //     },
-//   });
+//     767: {
+//       slidesPerView: 2.6,
+//     },
+//     320: {
+//       slidesPerView: 1.2,
+//     },
+//   },
 // });
+
+$(".projects__container").each(function () {
+  let $this = $(this);
+
+  new Swiper($this, {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    grabCursor: true,
+    freeMode: true,
+    breakpoints: {
+      1199: {
+        slidesPerView: 2,
+      },
+      767: {
+        slidesPerView: 1,
+      },
+      320: {
+        slidesPerView: 1,
+      },
+    },
+    navigation: {
+      nextEl: ".projects__next",
+      prevEl: ".projects__prev",
+    },
+    pagination: {
+      el: ".projects__progress",
+      type: "progressbar",
+    },
+  });
+});
 
 // Burger menu
 $(".header__burger").on("click", function () {
