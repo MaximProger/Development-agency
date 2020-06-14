da54fca4a03ddde3a2f187271bf67b8672daaf40
Maksim Koshman <maksimkoshman090@gmail.com>
2020-06-14 10:08:01 +0300

    create index page

---
+++ js/_custom.js
@@ -0,0 +1,87 @@
+// Intro Slider
+var galleryThumbs = new Swiper(".intro__thumbs", {
+  spaceBetween: 4,
+  slidesPerView: 4,
+  //   loop: true,
+  //   freeMode: true,
+  loopedSlides: 5, //looped slides should be the same
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper(".intro__slider", {
+  spaceBetween: 4,
+  loop: true,
+  navigation: {
+    nextEl: ".intro__next",
+    prevEl: ".intro__prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
+
+// Employees Slider
+var swiper = new Swiper(".employees__container", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".employees__next",
+    prevEl: ".employees__prev",
+  },
+  pagination: {
+    el: ".employees__progress",
+    type: "progressbar",
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 4,
+    },
+    767: {
+      slidesPerView: 3,
+    },
+    320: {
+      slidesPerView: 1.3,
+    },
+  },
+});
+
+// Project Slider
+var projectsSlider = new Swiper(".projects__container", {
+  slidesPerView: 2.6,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".projects__next",
+    prevEl: ".projects__prev",
+  },
+  pagination: {
+    el: ".projects__progress",
+    type: "progressbar",
+  },
+  breakpoints: {
+    1199: {
+      slidesPerView: 2.6,
+    },
+    767: {
+      slidesPerView: 2.6,
+    },
+    320: {
+      slidesPerView: 1.2,
+    },
+  },
+});
+
+// Burger menu
+$(".header__burger").on("click", function () {
+  $(this).toggleClass("active");
+  $(".header__nav").toggleClass("active");
+  $(".header").toggleClass("active");
+});
+
+// Show more -- Agency
+$(".agency__link").on("click", function (event) {
+  event.preventDefault();
+  $(this).hide();
+  $(".agency__text").removeClass("hidden");
+});
